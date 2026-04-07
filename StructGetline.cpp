05f0e4d396ae1d5c3b5e10d345c52e0fb9d984ab
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-04-07 15:40:33 +0700

    int main

--- StructGetline.cpp
+++ StructGetline.cpp
@@ -10,3 +10,23 @@ struct orang {
     detailAlamat alamat;
     int umur;
 };
+int main() {
+    orang mhs;
+    cout << "Nama Mahasiswa : ";
+    getline(cin, mhs.nama);
+
+    cout << "Alamat Desa : ";
+    getline(cin, mhs.alamat.desa);
+
+    cout << "Alamat Kota : ";
+    getline(cin, mhs.alamat.kota);
+
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
