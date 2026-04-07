7478797ef4a800aaa50dffa155ee048cb42ed400
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-04-07 15:10:54 +0700

    x = dataku

--- Array.cpp
+++ Array.cpp
@@ -6,9 +6,12 @@ int main() {
     int dataku[5] = {10,20,30,40,50};
     //deklarasi tanpa insialisasi
     int datamu[5];
-     int x;
+    int x;
 
     cout << "data ke -3 : " << dataku[2] << endl;
-    
-   
+
+      x = dataku[2];
+    dataku[2] = 600;
+    cout << "data ke -3 : " << dataku[2] << endl;
+
 }
